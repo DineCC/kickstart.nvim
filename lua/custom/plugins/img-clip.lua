5dfcf365c4ca99226a28d8db78b5a1c48c7c5487
DineCC <233103744+DineCC@users.noreply.github.com>
2025-11-01 22:45:10 +0800

    img-clip.nvim config change
    Create a copy of the images and put them into 'assets' folder in the
    same path with the current buffer.

--- lua/custom/plugins/img-clip.lua
+++ lua/custom/plugins/img-clip.lua
@@ -2,11 +2,18 @@ return {
   'HakonHarnes/img-clip.nvim',
   event = 'VeryLazy',
   opts = {
+    default = {
+      -- * dir_path setting must be inside 'default' to work
+      dir_path = function()
+        return vim.fn.expand '%:h' .. '/assets'
+      end,
+    },
+
     -- drag and drop options
     drag_and_drop = {
       enabled = true, ---@type boolean | fun(): boolean
       insert_mode = false, ---@type boolean | fun(): boolean
-      -- copy the image into the DIR (default: "assets"), then link to the copy
+      -- copy the image into the dir_path, then link to the copy
       copy_images = true,
     },
   },
