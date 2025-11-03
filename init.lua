6dd1e826c4cb43e7e81b74da2032dc70c56e76e2
DineCC <233103744+DineCC@users.noreply.github.com>
2025-11-03 20:14:18 +0800

    Added harper-ls
    A grammar checker.

--- init.lua
+++ init.lua
@@ -850,6 +850,8 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         codelldb = {},
+        ['harper-ls'] = {},
+
         -- zk = {}, -- seem not needed since zk cli is installed and we need zk cli to create new notebooks anyway
 
         clangd = {},
